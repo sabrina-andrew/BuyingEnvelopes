62a535b8557d2dd7d07d2f2891be8551fc83b181
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-20 15:05:38 -0500

    First Commit

---
+++ BuyingEnvelopes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BuyingEnvelopes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //How many boxes of envelopes can you buy with \$12 if one box costs 3?
+
+            int envelopesCost = 3;
+            int inWallet = 12;
+            int howManyBoxes = inWallet / envelopesCost;
+            Console.WriteLine("You can buy " + howManyBoxes + " boxes of envelopes.");
+        
+        }
+    }
+}
